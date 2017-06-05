2f05d16aec87db4e6a1d4298735c4b4f4af98193
Adrian Gil <adrian@giakonda.com>
2017-06-05 15:48:07 +0100

    eeee por fin he conseguido que me funcione lo de a;adir de forma dinamica dominios

--- dominios.php
+++ dominios.php
@@ -2,13 +2,14 @@
 <?php 
     session_start();
     require 'funcion.php';
-	$token = $_SESSION['token'];
+
 	if((!isset($_SESSION['email'])) && (empty($_SESSION['email']))) {
 		header('Location: login.php');
 	 }
 	if (empty($_SESSION['token'])) { // Para evitar ataques CSRF
 		$_SESSION['token'] = bin2hex(random_bytes(32));
 	}
+    $token = $_SESSION['token'];
     $consulta = new Consultas;
 ?>
 <html>
@@ -21,7 +22,12 @@
 
         <script src="js/jquery.js"></script>
 		<script src="js/bootstrap.js"></script>
-
+        <script>
+            function nuevoDominio() {
+                var nuevoCampo = "<td></td><td><div class=\"form-group\><form method=\"post\" autocomplete=\"off\"><input type=\"text\ name=\"dominio\" class=\"form-group\" maxLength=\"50\" placeholder=\"Dominio\"></div></td><td><button type=\"submit\" formaction=\"dominios/nuevo.php\" formmethod=\"post\" class=\"btn btn-scondary btn-sm\">A;adir</button></td>"
+                document.getElementById("dominio").innerHTML = nuevoCampo;
+            }
+        </script>
         <title>Usuarios</title>
     </head>
     <body>
@@ -29,6 +35,7 @@
             <div class="row">
                 <?php include 'sidebar.php'; ?>
                 <main class="col-sm-9 offset-sm-3 col-md-9 offset-md-2 pt-3">
+                <label>No olvides a;adir un registro A y un registro MX en tu servidor DNS que apunte a este servidor de correo para que funcione en tu dominio. Un registro SPF no es necesario pero es recomendable para evitar el spam. <a href="https://mediatemple.net/community/products/dv/204404314/how-can-i-create-an-spf-record-for-my-domain">Lee más sobre cómo crear registros SPF aquí.</a></label>
                     <form action="eliminar.php" method="post">
                         <table class="table">
                             <thead>
@@ -48,14 +55,14 @@
     
                                             }
                                 ?>
-
+                             <tr id="dominio"></tr>           
                             </tbody>
                             <tfoot>
-                                <tr><td colspan="3"><a href="nuevodominio.php"> <i class="fa fa-plus" aria-hidden="true"> </i> Añadir dominio</a> </td></tr>
+                                <tr><td colspan="3"> <button type="button" class="btn btn-secondary btn-sm" onclick="return nuevoDominio();"><i class="fa fa-plus"  aria-hidden="true"> </i> Añadir dominio </button></td></tr>
                             </tfoot>
                         </table>
                         <input type="hidden" name="token" value="<?php echo $token; ?>">
-                        <button type="submit" formmethod="post" id="boton" formaction="dominios/eliminar.php" class="btn btn-secondary btn-lg" disabled>Eliminar</button>
+                        <button type="submit" onclick="return confirm('NOTA: Eliminar un dominio eliminará los usuarios asociados a este. Está seguro?')" formmethod="post" id="boton" formaction="dominios/eliminar.php" class="btn btn-secondary btn-lg" disabled>Eliminar</button>
                     </form>
                 </main>
             </div>
